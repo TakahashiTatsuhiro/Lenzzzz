0fbd7d6855a175611e2791444e64e8b6d4898435
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-11-28 23:00:52 +0900

    login fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
 import { useState } from 'react';
 import './App.css';
-// import { Router, Route, Routes } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import ItemsList from './components/ItemsList';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
-
 function App() {
-	// const [mode, setMode] = useState('login');
-
 	return (
 		<AuthProvider>
-			{/* <Router> */}
+			<Router>
 				<Routes>
-					{/* <Route path='/' component={<LoginForm />} /> */}
-					<Route path='/' element={LoginForm} />
-					<Route path='/login' element={<LoginForm />} />
-					<Route path='/items' element={<ProtectedRoute><ItemsList /></ProtectedRoute> } />
+					<Route exact path='/' element={<LoginForm />} />
+					<Route exact path='/login' element={<LoginForm />} />
+					<Route
+						path='/items'
+						element={
+							<ProtectedRoute>
+								<ItemsList />
+							</ProtectedRoute>
+						}
+					/>
 				</Routes>
-			{/* </Router> */}
+			</Router>
 		</AuthProvider>
 	);
 }
--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,47 +1,65 @@
 import { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 const LoginForm = () => {
-    const navigate = useNavigate();
+	const [user_name, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const navigate = useNavigate();
     const { login, setUserId, setUserName } = useAuth();
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
 
-    const handleSubmit = async() => {
-        try {
-            const response = await fetch('/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({username, password})
-            });
-            if(response.ok) {
-                const data = await response.json();
-                setUserId(data.user.id);
-                setUserName(data.user.username);
+	const handleSubmit = async () => {
+		try {
+			const response = await fetch('/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ username: user_name, password }),
+			});
+			if (response.ok) {
+				const data = await response.json();
+                setUserId(data[0].id);
+                setUserName(user_name);
                 login();
-                navigate('/items');
-            }
-        }
-        catch {
+				navigate('/items'); // ここでItemsListコンポーネントへ遷移
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-        }
-    }
+    const handleSubmitTest = async () => {
+		try {
+            setUserId(1);
+            setUserName(user_name);
+            login(); // ログイン状態を更新
+			navigate('/items'); // ここでItemsListコンポーネントへ遷移
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    return (
-        <div className='login__container'>
-					<div className='login__box'>
-						<h1>Lenzzzz へようこそ</h1>
-						<p>ユーザー名</p>
-						<input type='text' placeholder='ユーザー名を入力' onChange={(e) => setUsername(e.target.value) } />
-						<p>パスワード</p>
-						<input type='password' placeholder='パスワードを入力' onChange={(e) => setPassword(e.target.value) }/>
-						<button >ログイン</button>
-					</div>
-				</div>
-    )
-}
+	return (
+		<div className='login__container'>
+			<div className='login__box'>
+				<h1>Lenzzzz へようこそ</h1>
+				<p>ユーザー名</p>
+				<input
+					type='text'
+					placeholder='ユーザー名を入力'
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<p>パスワード</p>
+				<input
+					type='password'
+					placeholder='パスワードを入力'
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button onClick={handleSubmit}>ログイン</button>
+			</div>
+		</div>
+	);
+};
 
 export default LoginForm;
