724d2b0be73c63a3d1a7db97341a1050de2dca89
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-11-28 13:52:47 +0900

    login list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [mode, setMode] = useState('login');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			{mode === 'login' && (
+				<div className='login__container'>
+					<div className='login__box'>
+						<h1>Lenzzzz へようこそ</h1>
+						<p>ユーザー名</p>
+						<input type='text' placeholder='ユーザー名を入力' />
+						<p>パスワード</p>
+						<input type='password' placeholder='パスワードを入力' />
+						<button onClick={() => setMode('items')}>ログイン</button>
+					</div>
+				</div>
+			)}
+			{mode === 'items' && (
+				<div className='items__container'>
+					<div className='items__header'>
+						<button>ホーム</button>
+						<button>検索</button>
+					</div>
+					<div className='items__add'>
+						<button>新しいアイテムを追加</button>
+					</div>
+					<div className='items__list'>
+						<div className='item'>
+							<img src='' alt='' />
+							<p>防災食</p>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
-export default App
+export default App;
